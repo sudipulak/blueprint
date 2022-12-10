d206c966d824a583420f49a3121374eec5ed3f41
sudipulak <sdpulak123@gmail.com>
2022-12-10 19:35:30 +0545

    bootstrap downgrade to 4.1.3 for tab responsiveness in iphone

--- js/script.js
+++ js/script.js
@@ -18,37 +18,37 @@ $(document).ready(function () {
     dots: true,
     autoHeight: true,
   });
+
+  // Wrap every letter in a span
+  var textWrapper = document.querySelector(".ml7 .letters");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+
+  var textWrapper = document.querySelector(".ml8 .letters");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  if (window.innerWidth > 600) {
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: ".ml7 .letter",
+        translateY: ["1.1em", 0],
+        duration: 800,
+        easing: "easeInOutQuad",
+        opacity: 1,
+        // delay: (el, i) => 50 * i,
+      })
+      .add({
+        targets: ".ml8 .letter",
+        translateY: ["1.1em", 0],
+        opacity: 1,
+        duration: 800,
+        easing: "easeInOutQuad",
+        // delay: (el, i) => 50 * i,
+      });
+  }
 });
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector(".ml7 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-
-var textWrapper = document.querySelector(".ml8 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-if (window.innerWidth > 600) {
-  anime
-    .timeline({ loop: false })
-    .add({
-      targets: ".ml7 .letter",
-      translateY: ["1.1em", 0],
-      duration: 800,
-      easing: "easeInOutQuad",
-      opacity: 1,
-      // delay: (el, i) => 50 * i,
-    })
-    .add({
-      targets: ".ml8 .letter",
-      translateY: ["1.1em", 0],
-      opacity: 1,
-      duration: 800,
-      easing: "easeInOutQuad",
-      // delay: (el, i) => 50 * i,
-    });
-}
